0a2e2bc424921e06e355509d11a63eb041b7f7a9
WenLong <midsu_wy@163.com>
2026-07-13 16:53:06 +0800

    新增my-pass服务

---
+++ my-pass/src/main/java/org/example/MyPassApplication.java
@@ -0,0 +1,16 @@
+package org.example;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@PropertySource("classpath:application.yml")
+public class MyPassApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyPassApplication.class, args);
+    }
+}
